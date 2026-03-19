6de7c57cfb4f451d2543cb3e111d2dd9af7d9fa2
Reeve-IT <dabreoreeve89@gmail.com>
2026-03-19 10:17:50 +0530

    Merged both files

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+#first activate venv by .\venv\Scripts\Activate
+
 import pandas as pd
 
 df=pd.read_csv("data/tmdb_5000_movies.csv")
-print(df.head())
-print(df.info())
-print(df.describe())
+credit=pd.read_csv("data/tmdb_5000_credits.csv")
+# print(df.head())
+# print(credit.head())
+# print(df.info())
+# print(df.describe())
+# print(df.isna().sum())
+
+#merging both csv
+merged_df=pd.merge(df,credit, on='title')
+
+#select only required columns
+merged_df=merged_df[['id','title','overview','genres','keywords','cast','crew']]
+
+#rename id to movie_id
+merged_df=merged_df.rename(columns={'id':'movie_id'})
+
+#drop rows with missing values
+merged_df=merged_df.dropna()
+
+#clean structure
+merged_df=merged_df.reset_index(drop=True)
+
+print(merged_df.head())
+print(merged_df.shape)
