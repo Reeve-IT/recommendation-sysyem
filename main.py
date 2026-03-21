1eda5972119a198d573ed2c74b63e9666023f689
Reeve-IT <dabreoreeve89@gmail.com>
2026-03-21 11:36:14 +0530

    converted into clean data

--- main.py
+++ main.py
@@ -63,4 +63,29 @@ def fetch_director(obj):
   return[]
 merged_df['crew']=merged_df['crew'].apply(fetch_director)
 
-print(merged_df[['genres','keywords','cast','crew']].head())
+#print(merged_df[['genres','keywords','cast','crew']].head())
+
+#remove spaces from names
+def remove_spaces(lst):
+  return[i.replace(" ","")for i in lst]
+merged_df['genres']=merged_df['genres'].apply(remove_spaces)
+merged_df['keywords']=merged_df['keywords'].apply(remove_spaces)
+merged_df['cast']=merged_df['cast'].apply(remove_spaces)
+merged_df['crew']=merged_df['crew'].apply(remove_spaces)
+
+#convert overview into list
+merged_df['overview']=merged_df['overview'].apply(lambda x:x.split())
+
+#create final tags column
+merged_df['tags']=merged_df['overview']+merged_df['genres']+merged_df['keywords']+merged_df['cast']+merged_df['crew']
+
+#convert list into string
+merged_df['tags']=merged_df['tags'].apply(lambda x:" ".join(x))
+
+#clean data set
+new_df=merged_df[['movie_id','title','tags']]
+
+#print(new_df.head())
+
+#convert into lower case 
+new_df['tags']=new_df['tags'].apply(lambda x: x.lower())
